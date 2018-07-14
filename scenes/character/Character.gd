4bc06537f668c8778e5649a0beeebeee61e4676b
bull0n <lucas.bulloni@protonmail.ch>
2018-07-14 11:10:43 +0200

    basic physic, tiletset and player animation

---
+++ scenes/character/Character.gd
@@ -0,0 +1,58 @@
+extends KinematicBody2D
+
+export(int) var maxSpeed = 800
+export(int) var friction = 0.4
+
+var velocity = Vector2();
+var acceleration = Vector2();
+
+const GRAVITY = 98
+const ACCEL = 1500
+const JUMP_ACC = -1300
+const UP = Vector2(0, -1)
+
+func _ready():
+	pass
+
+func _physics_process(delta):
+	acceleration.y = GRAVITY
+	
+	if Input.is_action_pressed("ui_right"):
+		acceleration.x = ACCEL
+	elif Input.is_action_pressed("ui_left"):
+		acceleration.x = -ACCEL
+	else:
+		acceleration.x = 0
+		
+	if is_on_floor():
+		if Input.is_action_pressed("ui_up"):
+			acceleration.y = JUMP_ACC
+	
+	velocity += acceleration
+	
+	if acceleration.x == 0:
+		velocity.x *= friction
+
+	velocity.x = clamp(velocity.x, -maxSpeed, maxSpeed)
+	
+	if velocity.length() < 0.2:
+		velocity = Vector2()
+
+	velocity = move_and_slide(velocity, UP)
+	animate()
+	
+	
+func animate():
+	if velocity.y > 0:
+		$Sprite.play("fall")
+	elif velocity.y < 0:
+		$Sprite.play("jump")
+	elif velocity.x > 0:
+		$Sprite.play("run")
+	elif velocity.x < 0:
+		$Sprite.play("run")
+	elif velocity.x == 0:
+		$Sprite.play("idle") 
+		
+	$Sprite.flip_h = velocity.x < 0
+	
